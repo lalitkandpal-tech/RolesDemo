35fb9e3b78016e3ed8d918636bffa76943e052a2
Lalit Kandpal <Lalit.Kandpal@HANDYGOIT.COM>
2020-05-06 14:53:19 +0530

    first commit

---
+++ RoleBasedAppAccess/Controllers/GetUsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RoleBasedAppAccess.Models;
+
+namespace RoleBasedAppAccess.Controllers
+{
+    public class GetUsersController : ApiController
+    {
+
+        //public HttpResponseMessage GetUsers()
+        //{
+            //List<RoleController> Userlist = new List<RoleController>();
+            //using (ApplicationDbContext dc = new ApplicationDbContext())
+            //{
+            //    Userlist = dc.Users.OrderBy(a => a.UserName).ToList();
+            //    HttpResponseMessage response;
+            //    response = Request.CreateResponse(HttpStatusCode.OK, Userlist);
+            //    return response;
+            //}
+        //}
+    }
+}
